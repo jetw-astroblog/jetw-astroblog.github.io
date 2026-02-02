02f2841332e176da70f8a05898536b85c9cc2bf1
jetw-astroblog <unisa.dady@gmail.com>
2026-02-02 11:28:22 +0000

    Update from https://github.com/jetw-astroblog/jet-w.astro-blog/commit/dd7aff4dce89a0d61aae0a1710b682ecf04a0a42

--- integration.js
+++
@@ -1,9 +0,0 @@
-import {
-  astroBlogIntegration,
-  integration_default
-} from "./chunk-6D3XRDNY.js";
-import "./chunk-A2E2VSAQ.js";
-export {
-  astroBlogIntegration,
-  integration_default as default
-};
